2ea1dc4662ec03d9e7850fe339b631679589522a
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-07-04 18:11:39 +0200

    [UPD] Aggiunta la home.blade.php e rimossa la welcome prcedente

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizio laravel-primi-passi</title>
+</head>
+<body>
+    <main> 
+        <section>
+            <h1>Hello World!</h1>
+        </section>
+    </main>
+</body>
+</html>
